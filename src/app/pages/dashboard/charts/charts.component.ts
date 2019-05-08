5777d714cc7463609f4192b50af26a980ed24045
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-05-08 12:00:07 -0400

    hid ngx-charts to fix the error

--- src/app/pages/dashboard/charts/charts.component.ts
+++ src/app/pages/dashboard/charts/charts.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-
+import { chartData } from './chart-data';
 
 @Component({
   selector: 'ngx-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.scss']
 })
-export class ChartsComponent implements OnInit {
+export class ChartsComponent {
 
-  constructor() { }
+  chartData: any[];
 
-  ngOnInit() {
+  view: any[] = [230, 100];
+  
+  //options
+  showXAxis = false;
+  showYAxis = false;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = true;
+  xAxisLabel = 'User';
+  showYAxisLabel = true;
+  yAxisLabel = 'Score';
+
+  colorScheme = {
+    domain:['#5C6670','#5C6670','#5C6670','#5C6670']
+  };
+
+  constructor(){
+    Object.assign(this, { chartData })
+  }
+
+  onSelect(event){
+    console.log(event);
   }
 
 }
