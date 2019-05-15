afb3caebdf9ae74742e6dfd651594257176af60b
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-05-15 12:13:39 -0400

    updated

--- src/app/pages/admin-lectures/admin-lectures.component.ts
+++ src/app/pages/admin-lectures/admin-lectures.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'ngx-admin-lectures',
@@ -7,8 +8,20 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./admin-lectures.component.scss']
 })
 export class AdminLecturesComponent {
-
-  selectedItemNgModel;
-
-  selectedItemFormControl = new FormControl();
+  
+  movies = [
+    '1. How to upgrade to Angular 7',
+    '2. Angular 7 CLI Prompts',
+    '3. Application Performance',
+    '4. Documentation Updates',
+    '5. Dependency Updates',
+    '6. Drag and Drop',
+    '7. Virtual Scrolling',
+    '8. Improved Accessibility of Selects',
+    '9. Partner Launches',
+    '10. Angular Elements'
+  ];
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 }
---
+++ src/app/pages/admin-lectures/admin-lectures.module.ts
@@ -0,0 +1,28 @@
+'@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ThemeModule } from '../../@theme/theme.module';
+import { AdminLecturesComponent } from './admin-lectures.component';
+import { HttpClientModule} from '@angular/common/http';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../../app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  imports: [
+    ThemeModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    DragDropModule
+  ],
+  exports:[
+      BrowserAnimationsModule
+  ],
+  declarations: [
+    AdminLecturesComponent,
+  ],
+})
+export class AdminLectureseModule { }
