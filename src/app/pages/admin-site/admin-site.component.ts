0d782bc874d70911040c2d35e85c816855e727d0
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-05-26 14:18:22 -0400

    updated styling

--- src/app/pages/admin-site/admin-site.component.ts
+++ src/app/pages/admin-site/admin-site.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'ngx-admin-site',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './admin-site.component.html',
 })
 export class AdminSiteComponent {
+  selectedFile: File
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  onFileChanged(event) {
+    const file = event.target.files[0]
+  }
+
+  onUpload() {
+    const uploadData = new FormData();
+    uploadData.append('myFile', this.selectedFile, this.selectedFile.name);
+    this.http.post('my-backend.com/file-upload', uploadData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe(event => {
+        console.log(event); // handle event here
+      });
+  }
 
 }
 
