2c426daee39552df903e4bdf815258ddf417859b
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-05-29 16:00:33 -0400

    updated

--- src/app/pages/dashboard/demographic/demographic.component.ts
+++ src/app/pages/dashboard/demographic/demographic.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
+import { DropdownService } from '../../../services/dropdown.service';
+import { HttpClient, HttpErrorResponse, HttpHeaders  } from '@angular/common/http';
+import { title} from '../../../interfaces/title';
 
 @Component({
   selector: 'ngx-demographic',
-  template: `
+  templateUrl: './demographic.component.html',
+  /*template: `
     <chart type="pie" [data]="data" [options]="options"></chart>
-  `,
+  `,*/
   styleUrls: ['./demographic.component.scss']
 })
 export class DemographicComponent implements OnDestroy {
@@ -13,8 +17,27 @@ export class DemographicComponent implements OnDestroy {
   data: any;
   options: any;
   themeSubscription: any;
+  titleList:any;
+  title_id: any;
+  userTitle: String [];
+  selected = null;
 
-  constructor(private theme: NbThemeService) {
+  arrTitle: any [] = [
+    {id:'BE',name: 'BE'},
+    {id:'BS',name: 'BS'},
+    {id:'DEPT BA' ,name: 'DEPT BA'},
+    {id:'DM',name: 'DM'},
+    {id:'EA BC' ,name: 'EA BC'},
+    {id:'EA FT' ,name: 'EA FT'},
+    {id:'EA PT' ,name: 'EA PT'},
+    {id:'MB' ,name: 'MB'}
+  ];
+
+  keyToken = 'blfyjKOdJPCTESy5zbC394VYYxzXnB21';
+
+  constructor(private theme: NbThemeService, private dropdownService: DropdownService, private http: HttpClient) {
+    this.getTitleList();
+
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
 
       const colors: any = config.variables;
@@ -52,6 +75,33 @@ export class DemographicComponent implements OnDestroy {
         },
       };
     });
+  }
+
+  getTitleList() {
+    
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://eadev.edgagement.com/api/user/type', {
+      'headers': new HttpHeaders().set('Authorization', this.keyToken)
+
+    })
+    .subscribe(
+      data => {
+        this.titleList = data["data"];
+        console.log("this titleList: "+ JSON.stringify(this.titleList));
+        this.userTitle = Object.keys(this.titleList);
+        //let keys = Object.keys(this.titleList);
+        //console.log("this titleList key--: "+ keys);
+        //let values = Object.values(this.titleList);
+        console.log("this titleList key--: "+ this.userTitle);
+    
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+    
   }
 
   ngOnDestroy(): void {
