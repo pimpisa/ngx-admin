ecfc3ac21f5ded4d365e5540594b4a94db54d851
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-06-04 16:03:21 -0400

    updated graph

--- src/app/pages/dashboard/report/report.component.ts
+++ src/app/pages/dashboard/report/report.component.ts
@@ -1,7 +1,7 @@
-import { Component, AfterContentInit, OnDestroy  } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy  } from '@angular/core';
 import { NgxGauge } from 'ngx-gauge/gauge/gauge';
 import { NgxGaugeModule } from 'ngx-gauge';
-import { NbThemeService } from '@nebular/theme';
+import { NbThemeService, NbColorHelper } from '@nebular/theme';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ChartOptions, ChartType, ChartDataSets} from '../../../../../node_modules/chart.js';
 
@@ -10,46 +10,175 @@ import { ChartOptions, ChartType, ChartDataSets} from '../../../../../node_modul
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss']
 })
-export class ReportComponent {
-  /*data: any;
+export class ReportComponent implements AfterViewInit, OnDestroy {
+  data: any;
   options: any;
   themeSubscription: any;
+  options_leaderboard: any;
   
   constructor(private theme: NbThemeService) {
+
   }
+
   ngAfterViewInit() {
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
 
       const colors = config.variables;
       const echarts: any = config.variables.echarts;
-      this.options = {
-        backgroundColor: echarts.bg,
-        color: [colors.warningLight, colors.infoLight, colors.dangerLight, colors.successLight, colors.primaryLight],
-        tooltip : {
-          formatter: "{a} <br/>{b} : {c}%"
-      },
-      series : [
-        {
-            type:'gauge',
-            detail : {formatter:'{value}%'},
-            startAngle:225,
-            endAngle:-45,
-            axisTick:{show:false},
-            pointer: {
-              width:50,
-              length: '50%',
-              color: 'rgb(169,169,169)'
-            },
-            data:[{value: 50, name: ''}]
+
+      var dataStyle = { 
+        normal: {
+            label : {
+                show: true,
+                position: 'insideRight',
+                formatter: '{c}%',
+              textStyle: {
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            fontStyle: 'normal',
+            fontWeight: 'bold',
+        },
+            }
         }
-    ]
-  };
+    };
+
+      this.options = {
+        tooltip: {
+          trigger: 'item'
+      },
+     
+      calculable: true,
+      grid: {
+          borderWidth: 0,
+          y: 80,
+          y2: 60
+      },
+      xAxis: [
+          {
+              type: 'category',
+              show: false,
+              data: ['Email', 'Text']
+          }
+      ],
+      yAxis: [
+          {
+              type: 'value',
+              show: false
+          }
+      ],
+      series: [
+          {
+              name: 'Push Messages',
+              type: 'bar',
+              itemStyle: {
+                  normal: {
+                      color: function(params) {
+                          // build a color map as your need.
+                          var colorList = [
+                            '#9fbeff','#75d6e8'
+                          ];
+                          return colorList[params.dataIndex]
+                      },
+                      label: {
+                          show: true,
+                          position: 'insideTop',
+                          formatter: '{c}%\n{b}',
+                          textStyle: {
+                fontFamily: 'Arial',
+                fontSize: 18,
+                fontStyle: 'normal',
+                fontWeight: 'bold',
+              },
+                      }
+                  }
+              },
+              data: [12,21],
+              markPoint: {
+                  tooltip: {
+                      trigger: 'item',
+                      backgroundColor: 'rgba(0,0,0,0)',
+                      formatter: function(params){
+                          return '<img src="' 
+                                  + params.data.symbol.replace('image://', '')
+                                  + '"/>';
+                      }
+                  },
+                  data: [
+                      {xAxis:0, y: 350, name:'Email', symbolSize:20, symbol: 'image://../asset/ico/折线图.png'},
+                      {xAxis:1, y: 350, name:'Text', symbolSize:20, symbol: 'image://../asset/ico/柱状图.png'},
+             
+                  ]
+              }
+          }
+      ]
+      };
+      //Leaderboard Rank
+      var dataStyle_Leaderboard = { 
+        normal: {
+            label : {
+                show: true,
+                position: 'insideRight',
+                formatter: '{c}%',
+              textStyle: {
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            fontStyle: 'normal',
+            fontWeight: 'bold',
+        },
+            }
+        }
+      };
+      this.options_leaderboard = {
+        tooltip : {
+          trigger: 'axis'
+      },
+      legend: {
+          data:['leaderboard']
+      },
+     
+      calculable : true,
+      xAxis : [
+          {
+              type : 'category',
+              data : ['User1','User2', 'User3','User4','User5'],
+              axisLine: 5
+          }
+      ],
+      yAxis : [
+          {
+              show : false,
+          }
+      ],
+      series : [
+          {
+              name:'User score',
+              type:'bar',
+              data:[100,80,60,40,20],
+              itemStyle: {
+                color:'#849EFE',
+              },
+              markPoint : {
+                  data : [
+                      {type : 'max', name: 'User1'},
+                      {type : 'min', name: 'User5'}
+                  ]
+              },
+              markLine : {
+                  data : [
+                      {type : 'average', name: 'Average'}
+                  ]
+              }
+          },
+        
+      ]
+      };
        
   });
   }
 
     ngOnDestroy(): void {
       this.themeSubscription.unsubscribe();
-    }*/
+    }
+  
 }
   
