e4d11e89a09ab80461774eb8075d2f08cddeda1a
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-06-14 17:17:37 -0400

    updated

--- src/app/pages/admin-site/admin-site.module.ts
+++ src/app/pages/admin-site/admin-site.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { ThemeModule } from '../../@theme/theme.module';
-import { AdminSiteComponent } from './admin-site.component';
+
 
 @NgModule({
   imports: [
     ThemeModule,
   ],
-  declarations: [
-    AdminSiteComponent,
-  ],
+  
 })
 export class AdminSiteModule { }
