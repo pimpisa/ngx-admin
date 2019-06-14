e4f0fe5c70aa5a4f1f5cefed3582ee580eb8ada4
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-06-14 16:53:03 -0400

    updated

--- src/app/pages/dashboard/charts/charts.module.ts
+++ src/app/pages/dashboard/charts/charts.module.ts
@@ -2,13 +2,11 @@ import { NgModule, Component, OnInit } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ChartsComponent} from './charts.component';
 import { NbThemeService, NbColorHelper } from '@nebular/theme';
-import { ChartModule } from 'angular2-chartjs';
 
 
 @NgModule({
   imports: [
     NgxChartsModule,
-    ChartModule,
   ],
   declarations: [
     ChartsComponent,
