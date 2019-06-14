50a85924790a1911ac4570534b3e4c303f37645e
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-06-14 16:40:57 -0400

    updated

--- src/app/pages/admin-game-demos/admin-game-demos.module.ts
+++ src/app/pages/admin-game-demos/admin-game-demos.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
-import { PreviewGameComponent } from './preview-game/preview-game.component';
 
 
 @NgModule({
   imports: [
     ThemeModule,
   
-  ],
-  declarations: [
-    PreviewGameComponent,
   ],
 })
 export class AdminGameDemosModule { 
