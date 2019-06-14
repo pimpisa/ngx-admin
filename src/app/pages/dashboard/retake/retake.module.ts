c3ed0a5e11dd5a2095ffb564c5d0330be3c71dc4
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-06-14 16:58:23 -0400

    updated

--- src/app/pages/dashboard/retake/retake.module.ts
+++ src/app/pages/dashboard/retake/retake.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ChartService } from '../../../services/chart.service';
-import { RetakeComponent } from '../retake/retake.component';
 import { HttpClientModule } from  '@angular/common/http';
 
 @NgModule({
 
-    declarations:[
-        RetakeComponent
-    ],
     imports: [
         BrowserModule,
         HttpClientModule
     ],
     providers:[ChartService],
-    bootstrap: [RetakeComponent]
+  
 
 })
 
