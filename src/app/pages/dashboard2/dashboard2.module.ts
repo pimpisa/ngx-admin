230f3fed85d89e088445adb1502ac4a5ff64b35d
Pimpisa Predaswad <pimpisapredaswad@Pimpisas-MacBook-Pro.local>
2019-06-25 17:29:11 -0400

    updated

--- src/app/pages/dashboard2/dashboard2.module.ts
+++ src/app/pages/dashboard2/dashboard2.module.ts
@@ -5,9 +5,10 @@ import { Dashboard2Component } from './dashboard2.component';
 import { PopularBoxComponent } from './popular-box/popular-box.component';
 import { CircleChartComponent } from './circle-chart/circle-chart.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
-  declarations: [Dashboard2Component, PopularBoxComponent, CircleChartComponent, BarChartComponent],
+  declarations: [Dashboard2Component, PopularBoxComponent, CircleChartComponent, BarChartComponent, ProgressBarComponent],
   imports: [
     ThemeModule
   ]
